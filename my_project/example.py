9c44c389414237c2ca12a803434f4a39126f1945
asera <tetsuchan78@gmail.com>
2025-07-21 10:47:29 +0900

    Step150 example.py

---
+++ my_project/example.py
@@ -0,0 +1,23 @@
+#課題１
+a ="Hello World"
+print(a)
+
+#課題２
+def greet():
+    print("こんにちは")
+greet()
+
+#課題３
+def print_name(name):
+    print(f"私の名前は{name}です")
+print_name("マッツ＠57期")
+
+#課題４
+def get_greet():
+    return "おはようございます！"
+print(get_greet())
+
+#課題５
+def add(a,b):
+    return a + b
+print(add(1, 2))
